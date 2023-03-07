8825a97071f057e3d5d196d7e65378e2a1022eff
brainahc <meet.malli11@gmail.com>
2023-03-08 00:06:24 +0530

    no change

--- rest_api.py
+++ rest_api.py
@@ -1,11 +1,11 @@
 import requests
-import json                 
+import json
 import csv
 
 base_url = 'https://reqres.in/api/users?page=2'
 resp = requests.get(base_url)
 print(resp.json())
 
-with open ('api33.csv', 'w') as optput_file:
+with open ('api1_3.csv', 'w') as optput_file:
     writer = csv.writer(optput_file)
     writer.writerow(resp)
